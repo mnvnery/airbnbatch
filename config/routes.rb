c140938d7382165f813ca868924fbe70f4433847
Sini Ninkovic <sini.sn.ninkovic@gmail.com>
2020-08-25 12:56:13 +0100

    completed setup of personalities model and controller (no delete)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  resources :personalities, only: [:index, :show, :new, :create, :update, :edit]
+  # do
+  #   resources :doses, only: [:new, :create]
+  # end
+  # resources :doses, only: [:destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
