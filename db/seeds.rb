41d2b09aa9013751207ca8ab5f7fdbca489d6190
Branca Chiotte <brancachiotte@gmail.com>
2020-08-27 18:28:43 +0100

    done lots of other things to

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,9 @@
 
 
 
-User.create!(first_name: 'Maria', last_name: 'Nery', location: '16 Villa Gaudelet, Paris', language: 'English, Portugese', phone: '+351 91 515 6924', email: 'mnvnery@gmail.com', password: 'test123', birthdate: Date.new(1997,7,23))
-User.create!(first_name: 'Sini', last_name: 'Ninkovic', location: '6049 Fairlane Drive, 94611, Oakland, CA, USA', language: 'English, German, Croatian', phone: '+1 510 220 9753', email: 'sini.sn.ninkovic@gmail.com', password: 'test123', birthdate: Date.new(1986,10,25))
-User.create!(first_name: 'Branca', last_name: 'Chiotte', location: 'Lisbon', language: 'English, Portugese', phone: '+351 917420504', email: 'brancachiotte@gmail.com', password: 'test123', birthdate: Date.new(1987,7,11))
+User.create!(first_name: 'Maria', last_name: 'Nery', location: '16 Villa Gaudelet, Paris', language: 'English, Portugese', phone: '+351 91 515 6924', email: 'mnvnery@gmail.com', password: 'test123', birthdate: Date.new(1997,7,23), image_url: 'https://avatars1.githubusercontent.com/u/67118326?v=4' )
+User.create!(first_name: 'Sini', last_name: 'Ninkovic', location: '6049 Fairlane Drive, 94611, Oakland, CA, USA', language: 'English, German, Croatian', phone: '+1 510 220 9753', email: 'sini.sn.ninkovic@gmail.com', password: 'test123', birthdate: Date.new(1986,10,25), image_url: 'https://avatars3.githubusercontent.com/u/67879815?v=4' )
+User.create!(first_name: 'Branca', last_name: 'Chiotte', location: 'Lisbon', language: 'English, Portugese', phone: '+351 917420504', email: 'brancachiotte@gmail.com', password: 'test123', birthdate: Date.new(1987,7,11), image_url: 'https://avatars2.githubusercontent.com/u/68083103?v=4' )
 
 # /personalities  GET personalities index
 # /personalities/:id  GET personalities show
