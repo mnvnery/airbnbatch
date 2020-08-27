afd831e6bf9b1ec94bc6cc5999c8ad2384336f41
Adriano Leite <adrianobleite@gmail.com>
2020-08-27 09:41:13 +0100

    Add new review link to bookings personality

--- app/controllers/personality_reviews_controller.rb
+++ app/controllers/personality_reviews_controller.rb
@@ -1,23 +1,25 @@
 class PersonalityReviewsController < ApplicationController
-
+  before_action :find_booking
   def new
-    @booking = Booking.find(params[:booking_id])
     @personality_review = PersonalityReview.new
   end
 
   def create
-    @booking = Booking.find(params[:booking_id])
     @personality_review = PersonalityReview.new(personality_review_params)
-    @personality_review.booking = @personality
+    @personality_review.booking = @booking
 
     if @personality_review.save
-      redirect_to booking_path(booking)
+      redirect_to booking_path(@booking)
     else
       render :new
     end
   end
 
   private
+
+  def find_booking
+    @booking = Booking.find(params[:booking_id])
+  end
 
   def personality_review_params
     params.require(:personality_review).permit(:comment, :rating)
