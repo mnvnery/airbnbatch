fea3b5820d06059bcc563f10473d3d12fd4ec78b
Maria Nery <mnvnery@gmail.com>
2020-08-28 16:32:48 +0100

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+PersonalityReview.destroy_all
+Booking.destroy_all
 Personality.destroy_all
 User.destroy_all
 
