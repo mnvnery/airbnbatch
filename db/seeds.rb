4e7a5b387892bff660cd6acf1f609964fd0f6536
Maria Nery <mnvnery@gmail.com>
2020-08-28 16:19:30 +0100

    seeds swapped

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-User.destroy_all
 Personality.destroy_all
+User.destroy_all
 
 User.create!(first_name: 'Maria', last_name: 'Nery', location: '16 Villa Gaudelet, Paris', language: 'English, Portugese', phone: '+351 91 515 6924', email: 'mnvnery@gmail.com', password: 'test123', birthdate: Date.new(1997,7,23), image_url: 'https://avatars1.githubusercontent.com/u/67118326?v=4' )
 User.create!(first_name: 'Sini', last_name: 'Ninkovic', location: '6049 Fairlane Drive, 94611, Oakland, CA, USA', language: 'English, German, Croatian', phone: '+1 510 220 9753', email: 'sini.sn.ninkovic@gmail.com', password: 'test123', birthdate: Date.new(1986,10,25), image_url: 'https://avatars3.githubusercontent.com/u/67879815?v=4' )
